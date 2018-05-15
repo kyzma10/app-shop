320c508c8f8a97ab83d9599674a278625bd1440b
kyzma10 <alkovalzp@ukr.net>
2018-05-15 19:44:26 +0300

    make template

---
+++ src/app/Regester/regester.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regester',
+  templateUrl: './regester.component.html',
+  styleUrls: ['./regester.component.scss']
+})
+
+export class Regester {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Home } from './Home/home.component';
+import { Login } from './Login/login.component';
+import { Regester } from './Regester/regester.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Home,
+    Login,
+    Regester
   ],
   imports: [
     BrowserModule
