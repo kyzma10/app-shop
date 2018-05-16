08d26d1bc73a06b48073c4516795f470b4045c77
kyzma10 <alkovalzp@ukr.net>
2018-05-16 13:58:05 +0300

    add page in app

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 
 export class Home {
+  currentSlide = 0;
 
+  handleClick() {
+    let items = document.getElementsByClassName('slider__item');
+      items[this.currentSlide].className = 'slider__item';
+      this.currentSlide = (this.currentSlide + 1) % items.length;
+      items[this.currentSlide].className = 'slider__item slider__show';
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { Home } from './Home/home.component';
 import { Login } from './Login/login.component';
 import { Regester } from './Regester/regester.component';
+import { Header } from './Shared/Header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Home,
     Login,
-    Regester
+    Regester,
+    Header
   ],
   imports: [
     BrowserModule
