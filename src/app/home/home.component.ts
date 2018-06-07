0061556640a38059dac7573a5420a945fdc274af
kyzma10 <alkovalzp@ukr.net>
2018-06-07 13:07:37 +0300

    changed slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,21 @@ import { Component } from '@angular/core';
 })
 
 export class HomeComponent {
-  currentSlide = 0;
+  currentSlide: number = 0;
+
+  slides: Array<any> = [
+    {path: '../../assets/img/item-1.png', status: true},
+    {path: '../../assets/img/item-2.png', status: false},
+    {path: '../../assets/img/item-3.png', status: false}
+  ];
 
   handleClick() {
-    let items = document.getElementsByClassName('slider__item');
-      items[this.currentSlide].className = 'slider__item';
-      this.currentSlide = (this.currentSlide + 1) % items.length;
-      items[this.currentSlide].className = 'slider__item slider__show';
-  };
+    let currentItem = (this.currentSlide + 1) % this.slides.length;
+    for(let i = 0; i < this.slides.length; i++) {
+      if (i === currentItem) {
+        this.slides[i].status = true;
+      } else this.slides[i].status = false;
+    }
+    this.currentSlide++;
+  }
 }
